e7a9324de73836f2b36f68868480c1a9d259bf2c
07devAdvb <davidvivarbogonez@gmail.com>
2024-10-18 17:28:50 +0200

    Final version

--- README.md
+++ README.md
@@ -1,17 +1,18 @@
 # hsmatrix
-An alternative to the famous program written in C++ `cmatrix`.
+An alternative to the famous program written in C: `cmatrix`.
 
 ## Usage
 ```bash
 cabal run
 ```
 > [!NOTE]
-> For now this program is only tested on Linux.
-> And it doesn't have the full functionality of `cmatrix` yet.
+> This program is only tested on Linux.
+> And it doesn't have the full functionality of `cmatrix`.
 
 > [!IMPORTANT]
 > You need to have installed:
 > - `cabal`
 > - `stack`
 > - `ghc`
+
 > In order to run this program.
--- app/Main.hs
+++ app/Main.hs
@@ -1,126 +1,60 @@
 module Main where
 
-import Control.Concurrent
-import Control.Monad
-import Data.List ()
+import Control.Concurrent (threadDelay)
+import Control.Monad.State
+import Data.List (transpose)
 import System.Console.ANSI
 import qualified System.Console.Terminal.Size as TS
 import System.IO (hFlush, stdout)
 import System.Random
 
-data Cell = Cell
-  { charIndex :: Int,
-    intensity :: Int -- 0: Inactive, 1-5: Active (5 being the brightest)
-  }
-  deriving (Eq)
+import Init
+import Types
+import Update
 
-type Column = [Cell]
-
-type Matrix = [Column]
-
-chars :: String
-chars = ['ｱ', 'ｲ', 'ｳ', 'ｴ', 'ｵ', 'ｶ', 'ｷ', 'ｸ', 'ｹ', 'ｺ', 'ｻ', 'ｼ', 'ｽ', 'ｾ', 'ｿ', 'ﾀ', 'ﾁ', 'ﾂ', 'ﾃ', 'ﾄ', 'ﾅ', 'ﾆ', 'ﾇ', 'ﾈ', 'ﾉ', 'ﾊ', 'ﾋ', 'ﾌ', 'ﾍ', 'ﾎ', 'ﾏ', 'ﾐ', 'ﾑ', 'ﾒ', 'ﾓ', 'ﾔ', 'ﾕ', 'ﾖ', 'ﾗ', 'ﾘ', 'ﾙ', 'ﾚ', 'ﾛ', 'ﾜ', 'ﾝ']
-
-initCell :: IO Cell
-initCell = do
-  charIdx <- randomRIO (0, length chars - 1)
-  return $ Cell charIdx 0
-
-initColumn :: Int -> IO Column
-initColumn h = replicateM h initCell
-
-initMatrix :: Int -> Int -> IO Matrix
-initMatrix w h = replicateM w (initColumn h)
-
--- Update a single column
-updateColumn :: Int -> Column -> IO Column
-updateColumn h col = do
-  shouldStartNewRain <- (== 0) <$> randomRIO (0, 20 :: Int)
-  newCharIdx <- randomRIO (0, length chars - 1)
-  let rainLength = randomRIO (5, 15) >>= \len -> return (min len h)
-  let activeLength = length $ takeWhile (\c -> intensity c > 0) col
-  newHead <- if shouldStartNewRain && activeLength == 0
-             then do
-               len <- rainLength
-               return $ replicate len (Cell newCharIdx 5) ++ replicate (h - len) (Cell 0 0)
-             else return [Cell 0 0]
-  return $ take h $ newHead ++ col
-
--- Update a single cell
-updateCell :: Cell -> Cell
-updateCell (Cell idx i)
-  | i > 0 = Cell idx (max 1 (i - 1))
-  | otherwise = Cell idx 0
-
--- Update the entire matrix
-updateMatrix :: Int -> Matrix -> IO Matrix
-updateMatrix h = mapM (updateColumn h)
-
--- Define custom color for fading effect
+-- | Determine the color of a cell based on its intensity
 fadeColor :: Int -> Color
 fadeColor i
   | i == 5 = White
   | i > 0 = Green
   | otherwise = Black
 
--- Render the entire matrix
-renderMatrix :: Int -> Int -> Matrix -> IO ()
-renderMatrix w h matrix = do
+-- | Render the matrix to the terminal
+renderMatrix :: Matrix -> IO ()
+renderMatrix matrix = do
   setCursorPosition 0 0
-  forM_ [0 .. h - 1] $ \y -> do
-    forM_ [0 .. w - 1] $ \x -> do
-      let cell = matrix !! x !! y
+  forM_ (transpose matrix) $ \row -> do
+    forM_ row $ \cell -> do
       setSGR [SetColor Foreground Vivid (fadeColor (intensity cell))]
-      if intensity cell > 0
-        then putChar (chars !! charIndex cell)
-        else putChar ' '
-    putChar '\n' -- Move to the next line after each row
+      putChar $ if intensity cell > 0 then chars !! charIndex cell else ' '
+    putChar '\n'
   hFlush stdout
 
--- Get terminal size
+-- | Get the current terminal size
 getCurrentTerminalSize :: IO (Int, Int)
 getCurrentTerminalSize = do
   maybeSize <- TS.size
   case maybeSize of
-    Just (TS.Window rows cols) -> return (cols, rows - 1) -- Subtract 1 from rows to prevent scrolling
-    Nothing -> return (80, 24) -- Default size if unable to get terminal size
+    Just (TS.Window rows cols) -> return (cols, rows - 1)
+    Nothing -> return (80, 24) -- Default size if unable to determine
 
-adjustMatrixSize :: Int -> Int -> Matrix -> IO Matrix
-adjustMatrixSize w h matrix = do
-  let currentW = length matrix
-  -- let currentH = if null matrix then 0 else length (head matrix)
-
-  newMatrix <-
-    if currentW /= w
-      then do
-        let commonCols = min currentW w
-        let existingCols = take commonCols matrix
-        newCols <- replicateM (max 0 (w - currentW)) (initColumn h)
-        return $ existingCols ++ newCols
-      else return matrix
-
-  -- Adjust the height of each column
-  return $ map (adjustColumnHeight h) newMatrix
-  where
-    adjustColumnHeight :: Int -> Column -> Column
-    adjustColumnHeight targetH col
-      | length col < targetH = col ++ replicate (targetH - length col) (Cell 0 0)
-      | length col > targetH = take targetH col
-      | otherwise = col
-
-mainLoop :: Matrix -> IO ()
-mainLoop matrix = do
+-- | Main loop for updating and rendering the matrix
+mainLoop :: Matrix -> StdGen -> IO ()
+mainLoop matrix gen = do
   (w, h) <- getCurrentTerminalSize
-  adjustedMatrix <- adjustMatrixSize w h matrix
-  newMatrix <- updateMatrix h adjustedMatrix
-  renderMatrix w h (map (map updateCell) newMatrix)
+  let (adjustedMatrix, gen1) = runState (adjustMatrixSize w h matrix) gen
+  let (newMatrix, gen2) = runState (updateMatrix h adjustedMatrix) gen1
+  let updatedMatrix = map (map updateCell) newMatrix
+  renderMatrix updatedMatrix
   threadDelay 50000 -- 50ms delay
-  mainLoop newMatrix
+  mainLoop updatedMatrix gen2
 
+-- | Main function to set up and start the matrix rain effect
 main :: IO ()
 main = do
   hideCursor
   clearScreen
   (w, h) <- getCurrentTerminalSize
-  matrix <- initMatrix w h
-  mainLoop matrix
+  gen <- getStdGen
+  let (initialMatrix, initialGen) = runState (initMatrix w h) gen
+  mainLoop initialMatrix initialGen
